88baef4513609463fdb426581668197ae651f9b5
Victor Matsakov <victor.matsakov7@gmail.com>
2023-04-12 20:13:25 +0400

    готовые слайдеры

--- js/app.js
+++ js/app.js
@@ -1,6 +1,62 @@
+$(function() {
+
+/* Slider */
 $('.slick-item').slick({
     arrows: false,
     fade: true,
     autoplay: true,
     autoplaySpeed: 4000
+});
+
+
+/* Slider navigation */
+$('#slickSliderNext').on('click', function() {
+    $('.slick-item').slick('slickPrev')
+});
+
+$('#slickSliderPref').on('click', function() {
+    $('.slick-item').slick('slickNext')
+});
+
+/* ***************** */
+
+/* Slider 2*/
+$('.reviews__slider').slick({
+  centerMode: true,
+  centerPadding: '22%',
+  slidesToShow: 1,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+/* Slider2 navigation */
+$('#slickReviewsPrev').on('click', function() {
+  $('.reviews__slider').slick('slickPrev')
+});
+
+$('#slickReviewsNext').on('click', function() {
+  $('.reviews__slider').slick('slickNext')
+});
+
+
+
 });
